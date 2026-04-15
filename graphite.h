303951dfeba54db24790d57a2a92be5e93c23774
Nico Russo <nicojrusso@gmail.com>
2026-04-14 23:23:22 -0400

    Initial Commit

---
+++ graphite.h
@@ -0,0 +1,8 @@
+//
+// Created by Nico Russo on 4/14/26.
+//
+
+#ifndef GRAPHITE_H
+#define GRAPHITE_H
+
+#endif //GRAPHITE_H
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "graphite.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
